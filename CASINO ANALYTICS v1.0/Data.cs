24bdd74248af9f5af0f0b921a34a17ad65931e10
Danilo Milosevic <dmilosevic@live.com>
2015-12-02 23:00:01 +0100

    peder

--- CASINO ANALYTICS v1.0/Data.cs
+++ CASINO ANALYTICS v1.0/Data.cs
@@ -19,6 +19,8 @@ namespace CASINO_ANALYTICS_v1._0
         public int headcount;
 
         //puluj ovo pederu
+
+            //evo jos jednog pedera
         public Data(string user, string tablename, int year, int month, int day, int fromH, int toH, double drop, double result, int headcount)
         {
             this.user = user;
