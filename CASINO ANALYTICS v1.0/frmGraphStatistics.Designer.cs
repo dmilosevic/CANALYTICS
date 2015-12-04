005c5666ffb617b1f58d4af24fd73cd7152e8dab
Ognjen Gatalo <ognjeng95@gmail.com>
2015-12-04 02:17:59 +0100

    adeed graph stats form

---
+++ CASINO ANALYTICS v1.0/frmGraphStatistics.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CASINO_ANALYTICS_v1._0
+{
+    partial class frmGraphStatistics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmGraphStatistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 500);
+            this.Name = "frmGraphStatistics";
+            this.Text = "frmGraphStatistics";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
